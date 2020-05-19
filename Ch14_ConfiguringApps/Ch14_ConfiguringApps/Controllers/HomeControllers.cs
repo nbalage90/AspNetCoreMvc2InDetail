a77b539a71ce90e1988722823b564287a52742a4
Németh Balázs <nemethbalazs.nb@gmail.com>
2020-05-19 07:55:38 +0200

    Chapter14 - Configuration

---
+++ Ch14_ConfiguringApps/Ch14_ConfiguringApps/Controllers/HomeControllers.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ch14_ConfiguringApps.Controllers
+{
+    public class HomeControllers : Controller
+    {
+        public ViewResult Index() => View(new Dictionary<string, string>
+        {
+            ["Message"] = "This is the Index action"
+        });
+    }
+}
