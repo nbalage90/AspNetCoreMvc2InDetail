2998ca9600afaab87b1435772a31d1208d6e0934
bane01 <balazs.nemeth@betssongroup.com>
2020-06-16 15:48:53 +0200

    Bootstrap

--- Ch14_ConfiguringApps/Ch15_URLRouting/Startup.cs
+++ Ch14_ConfiguringApps/Ch15_URLRouting/Startup.cs
@@ -27,7 +27,12 @@ namespace Ch15_URLRouting
             app.UseDeveloperExceptionPage();
             app.UseStatusCodePages();
             app.UseStaticFiles();
-            app.UseMvcWithDefaultRoute();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
             //app.UseMvc(routes =>
             //{
             //    routes.MapRoute(
