42a77250bdd1ff68cc4de11f74720d12e211310c
bane01 <balazs.nemeth@betssongroup.com>
2020-06-16 11:51:11 +0200

    End of Chapter 15

--- Ch14_ConfiguringApps/Ch15_URLRouting/Controllers/CustomerController.cs
+++ Ch14_ConfiguringApps/Ch15_URLRouting/Controllers/CustomerController.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 
 namespace Ch15_URLRouting.Controllers
 {
+    [Route("app/[controller]/actions/[action]/{id:weekday?}")]
     public class CustomerController : Controller
     {
         public ViewResult Index() =>
--- Ch14_ConfiguringApps/Ch15_URLRouting/Startup.cs
+++ Ch14_ConfiguringApps/Ch15_URLRouting/Startup.cs
@@ -2,9 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Ch15_URLRouting.Infrastructure;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
 using Microsoft.AspNetCore.Routing.Constraints;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -15,6 +17,8 @@ namespace Ch15_URLRouting
     {
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<RouteOptions>(options =>
+                options.ConstraintMap.Add("weekday", typeof(WeekDayContraint)));
             services.AddMvc(options => options.EnableEndpointRouting = false);
         }
 
@@ -23,42 +27,46 @@ namespace Ch15_URLRouting
             app.UseDeveloperExceptionPage();
             app.UseStatusCodePages();
             app.UseStaticFiles();
-            app.UseMvc(routes =>
-            {
-                routes.MapRoute(
-                    name: "MyRoute",
-                //  template: "{controller:regex(^H.*)=Home}/{action:regex(^Index|^About$)=Index}/{id?}");
-                    template: "{controller=Home}/{action=Index}/{id:range(10, 20)?}");
-                //routes.MapRoute(
-                //    name: "MyRoute2",
-                //    template: "{controller}/{action}/{id?}",
-                //    defaults: new { controller = "Home", action = "Index" },
-                //    constraints: new { id = new IntRouteConstraint() });
+            app.UseMvcWithDefaultRoute();
+            //app.UseMvc(routes =>
+            //{
+            //    routes.MapRoute(
+            //        name: "MyRoute",
+            //    //  template: "{controller:regex(^H.*)=Home}/{action:regex(^Index|^About$)=Index}/{id?}");
 
-                //routes.MapRoute(
-                //    name: "MyRoute",
-                //    template: "{controller=Home}/{action=Index}/{id?}/{*catchall}"); // catchall: variable segment, it could be i.e: Delete/Perm
+            //    //  template: "{controller=Home}/{action=Index}/{id:range(10, 20)?}");
 
-                //routes.MapRoute(
-                //    name: "ShopSchema2",
-                //    template: "Shop/OldAction",
-                //    defaults: new { controller = "Home", action = "Index" });
+            //        template: "{controller}/{action}/{id:weekday?}");
+            //    //routes.MapRoute(
+            //    //    name: "MyRoute2",
+            //    //    template: "{controller}/{action}/{id?}",
+            //    //    defaults: new { controller = "Home", action = "Index" },
+            //    //    constraints: new { id = new IntRouteConstraint() });
 
-                //routes.MapRoute(
-                //    name: "ShopSchema",
-                //    template: "Shop/{action}",
-                //    defaults: new { controller = "Home" });
+            //    //routes.MapRoute(
+            //    //    name: "MyRoute",
+            //    //    template: "{controller=Home}/{action=Index}/{id?}/{*catchall}"); // catchall: variable segment, it could be i.e: Delete/Perm
 
-                //routes.MapRoute("", "X{controller}/{action}");
+            //    //routes.MapRoute(
+            //    //    name: "ShopSchema2",
+            //    //    template: "Shop/OldAction",
+            //    //    defaults: new { controller = "Home", action = "Index" });
 
-                //routes.MapRoute(
-                //    name: "default", 
-                //    template: "{controller=Home}/{action=Index}");
+            //    //routes.MapRoute(
+            //    //    name: "ShopSchema",
+            //    //    template: "Shop/{action}",
+            //    //    defaults: new { controller = "Home" });
 
-                //routes.MapRoute(
-                //    name: "",
-                //    template: "Public/{controller=Home}/{action=Index}");
-            });
+            //    //routes.MapRoute("", "X{controller}/{action}");
+
+            //    //routes.MapRoute(
+            //    //    name: "default", 
+            //    //    template: "{controller=Home}/{action=Index}");
+
+            //    //routes.MapRoute(
+            //    //    name: "",
+            //    //    template: "Public/{controller=Home}/{action=Index}");
+            //});
         }
     }
 }
