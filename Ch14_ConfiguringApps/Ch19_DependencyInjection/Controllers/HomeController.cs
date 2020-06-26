371077e14c37cbde3be16c90acd677db3d8b29cb
bane01 <balazs.nemeth@betssongroup.com>
2020-06-26 13:09:33 +0200

    Chapter 19: begining...

---
+++ Ch14_ConfiguringApps/Ch19_DependencyInjection/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Ch19_DependencyInjection.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ch19_DependencyInjection.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IRepository repository;
+
+        public HomeController(IRepository repo) => repository = repo;
+
+        public ViewResult Index() => View(repository.Products);
+    }
+}
---
+++ Ch14_ConfiguringApps/Ch19_DependencyInjection/Startup.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Ch19_DependencyInjection.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Ch19_DependencyInjection
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddTransient<IRepository, MemoryRepository>();
+            services.AddMvc(options => options.EnableEndpointRouting = false);
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseStatusCodePages();
+            app.UseDeveloperExceptionPage();
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
+        }
+    }
+}
