ff784cf62a1b997eeff769a24770544fe0cdf41c
bane01 <balazs.nemeth@betssongroup.com>
2020-07-01 10:20:40 +0200

    Chapter 19

---
+++ Ch14_ConfiguringApps/Ch19_Filters/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using Ch19_Filters.Infrastructure;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ch19_Filters.Controllers
+{
+    [Profile]
+    [ViewResultDetails]
+    [RangeException]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("Message", "This is the Index action on the Home controller");
+        }
+
+        public IActionResult SecondAction()
+        {
+            return View("Message", "This is the SecondAction action on the Home controller");
+        }
+
+        public ViewResult GenerateException(int? id)
+        {
+            if (id == null)
+            {
+                throw new ArgumentNullException(nameof(id));
+            }
+            else if (id > 10)
+            {
+                throw new ArgumentOutOfRangeException(nameof(id));
+            }
+            else
+            {
+                return View("Message", $"The value is {id}");
+            }
+        }
+    }
+}
