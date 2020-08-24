4b3128bae93c3210f838960bc1ec3da9643937a3
bane01 <balazs.nemeth@betssongroup.com>
2020-08-24 09:50:40 +0200

    Chapter 23: Understanding TagHelpers

---
+++ Ch14_ConfiguringApps/Ch23_UnderstandingTagHelpers/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using Ch23_UnderstandingTagHelpers.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.ResponseCaching;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ch23_UnderstandingTagHelpers.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IRepository repository;
+
+        public HomeController(IRepository repo)
+        {
+            repository = repo;
+        }
+
+        public ViewResult Index() => View(repository.Cities);
+
+        public ViewResult Create() => View();
+
+        [HttpPost]
+        public IActionResult Create(City city)
+        {
+            repository.AddCity(city);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Ch14_ConfiguringApps/Ch23_UnderstandingTagHelpers/Infrastructure/TagHelpers/TableCellTagHelper.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using Microsoft.AspNetCore.Routing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ch23_UnderstandingTagHelpers.Infrastructure.TagHelpers
+{
+    [HtmlTargetElement("td", Attributes = "wrap")]
+    public class TableCellTagHelper : TagHelper
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.PreContent.SetHtmlContent("<b><i>");
+            output.PostContent.SetHtmlContent("</i></b>");
+        }
+    }
+}
