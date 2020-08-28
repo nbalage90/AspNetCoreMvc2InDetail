be2a56146c981724753237d324521463eba13f38
bane01 <balazs.nemeth@betssongroup.com>
2020-08-28 10:10:31 +0200

    Chapter 27

---
+++ Ch14_ConfiguringApps/Ch27_ModelValidation/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using Ch27_ModelValidation.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ch27_ModelValidation.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index() => View("MakeBooking", new Appointment { Date = DateTime.Now });
+
+        [HttpPost]
+        public ViewResult MakeBooking(Appointment appt)
+        {
+            if (ModelState.GetValidationState(nameof(appt.Date)) == ModelValidationState.Valid &&
+                ModelState.GetValidationState(nameof(appt.ClientName)) == ModelValidationState.Valid &&
+                appt.ClientName.Equals("Joe", StringComparison.OrdinalIgnoreCase) &&
+                appt.Date.DayOfWeek == DayOfWeek.Monday)
+            {
+                ModelState.AddModelError("", "Joe cannot book appointments on Mondays");
+            }
+
+            if (ModelState.IsValid)
+            {
+                return View("Completed", appt);
+            }
+            else
+            {
+                return View();
+            }
+        }
+        public JsonResult ValidateDate(string Date)
+        {
+            DateTime parsedDate;
+
+            if (!DateTime.TryParse(Date, out parsedDate))
+            {
+                return Json("Please enter a valid date (mm/dd/yyyy)");
+            }
+            else if (DateTime.Now > parsedDate)
+            {
+                return Json("Please enter a date in the future");
+            }
+            else
+            {
+                return Json(true);
+            }
+        }
+    }
+}
---
+++ Ch14_ConfiguringApps/Ch27_ModelValidation/Models/Appointment.cs
@@ -0,0 +1,25 @@
+﻿using Ch27_ModelValidation.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ch27_ModelValidation.Models
+{
+    public class Appointment
+    {
+        [Required]
+        [Display(Name = "name")]
+        public string ClientName { get; set; }
+
+        [UIHint("Date")]
+        [Required(ErrorMessage = "Please enter a date")]
+        [Remote("ValidateDate", "Home")]
+        public DateTime Date { get; set; }
+
+        [MustBeTrue(ErrorMessage = "You must accept the terms")]
+        public bool TermsAccepted { get; set; }
+    }
+}
